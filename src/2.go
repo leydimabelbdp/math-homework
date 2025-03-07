44e8906260ca2191117def19f786effc0952c1de
leydimabelbdp <leydimabelbdp@gmail.com>
2025-03-07 07:34:28 +0000

    Add 2.go

---
+++ src/2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare variables for the two numbers we want to add
+	var num1 int = 5
+	var num2 int = 3
+
+	// Add the two numbers together
+	result := num1 + num2
+
+	// Print the result
+	fmt.Println("The sum of", num1, "and", num2, "is", result)
+}
