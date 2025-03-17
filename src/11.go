69c467f4eda308e2c059784e07f3daf9f44e40a0
leydimabelbdp <leydimabelbdp@gmail.com>
2025-03-17 14:16:55 +0000

    Add 11.go

---
+++ src/11.go
@@ -0,0 +1,11 @@
+func main() {
+    // Homework problem 1
+    fmt.Println("Solve for x: 2x + 3 = 7")
+    x := 7 - 3
+    fmt.Println("The solution is", x)
+    
+    // Homework problem 2
+    fmt.Println("Solve for y: 4y - 12 = 20")
+    y := 20 + 12
+    fmt.Println("The solution is", y)
+}
